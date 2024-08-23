d4a305119d334bf786d9ae16837bb4ad1abadac0
BikmanDjuma <bikmangeek@gmail.com>
2024-08-23 22:03:48 +0200

    work on home page

--- resources/views/homePage/cover.blade.php
+++ resources/views/homePage/cover.blade.php
@@ -69,17 +69,16 @@
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-white mb-3">Quick Link</h4>
+                    <a class="btn btn-link" href="">Home</a>
                     <a class="btn btn-link" href="">About Us</a>
+                    <a class="btn btn-link" href="">Courses</a>
                     <a class="btn btn-link" href="">Contact Us</a>
-                    <a class="btn btn-link" href="">Privacy Policy</a>
-                    <a class="btn btn-link" href="">Terms & Condition</a>
-                    <a class="btn btn-link" href="">FAQs & Help</a>
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-white mb-3">Contact</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>24V3+JJH, Kigali</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>0782176615</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>mugemajames@gmail.com</p>
                     <div class="d-flex pt-2">
                         <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
@@ -124,10 +123,7 @@
             <div class="copyright">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
-
-                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
+                        &copy; <a class="border-bottom" href="#">{{ config('app.name','laravel') }}</a>, All Right Reserved {{ date('Y') }} . Designed By <a class="border-bottom" href="{{ route('home.page') }}">James Mugema</a>
                     </div>
                     <div class="col-md-6 text-center text-md-end">
                         <div class="footer-menu">
